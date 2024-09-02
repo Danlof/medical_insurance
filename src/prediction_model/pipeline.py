8d09e78462e75295153a86b9b12e4a714383edf4
danlof <danmusyoki.dk@gmail.com>
2024-09-02 19:44:27 +0300

    pipeline

--- src/prediction_model/pipeline.py
+++ src/prediction_model/pipeline.py
@@ -1,7 +1,14 @@
 from sklearn.pipeline import Pipeline
-import config
-import processing.preprocessing as pp
+from prediction_model import config
+import prediction_model.processing.preprocessing as pp
+from sklearn.ensemble import RandomForestRegressor
 
-pipeline = Pipeline([
+print("pipeline process has started")
+regressor_pipeline = Pipeline([
+    ("LabelEncoder", pp.CustomEncoder(variables=config.FEATURES_TO_ENCODE)),
+    ("LogTransformation", pp.LogTransforms(variables=config.NUM_FEATURES)),
+    ("Standardization", pp.Standardization(variables=config.NUM_FEATURES)),
+    ("RandomForestRegressor", RandomForestRegressor(random_state=1))
     
 ])
+print("pipeline process has ended ")
